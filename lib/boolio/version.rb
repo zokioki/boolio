a2647f21d7f2075e3e62ce11fdcdba0ce86b40b4
Zoran <zoran1991@gmail.com>
2016-04-12 03:09:44 -0700

    add tests

--- lib/boolio/version.rb
+++ lib/boolio/version.rb
@@ -1,3 +1,3 @@
-module Boolio
+class Boolio
   VERSION = "0.1.0"
 end
--- spec/boolio_spec.rb
+++ spec/boolio_spec.rb
@@ -1,11 +1,31 @@
-require 'spec_helper'
+require "spec_helper"
 
 describe Boolio do
-  it 'has a version number' do
+  let(:true_boolio)  { Boolio.new val: true }
+  let(:false_boolio) { Boolio.new val: false }
+
+  it "has a version number" do
     expect(Boolio::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it "has an endpoint URL constant" do
+    expect(Boolio::URL).to eq "https://api.booleans.io/"
+  end
+
+  it "throws exception if invalid argument type is provided" do
+    expect{ Boolio.new val: 3 }.to raise_error(ArgumentError)
+  end
+
+  it "takes value argument on initialize" do
+    expect(true_boolio.val).to eq true
+    expect(false_boolio.val).to eq false
+  end
+
+  it "assigns a random boolean if no value argument is passed" do
+    expect([true, false]).to include(Boolio.new().val)
+  end
+
+  it "assigns retreived ID from created boolean" do
+    expect(true_boolio.id).to be_instance_of String
   end
 end
