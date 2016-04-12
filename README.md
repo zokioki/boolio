0ea50574a1694ac4c23c4e7b7eb1157402dc7d5d
Zoran <zoran1991@gmail.com>
2016-04-12 02:37:50 -0700

    switch over from Net/HTTP; fix false value triggering fallback sample on init

--- README.md
+++ README.md
@@ -10,17 +10,47 @@ Add this line to your application's Gemfile:
 gem 'boolio'
 ```
 
-And then execute:
-
-    $ bundle
-
 Or install it yourself as:
 
     $ gem install boolio
 
 ## Usage
 
+Creating a Boolio instance hits the booleans.io API to create a corresponding boolean:
 
+```ruby
+bool = Boolio.new val: true
+# => #<Boolio:0x007fdcfa8f3548 @val=true, @id="1aa1f9c8-c65d-43c9-b8cb-ed171f0719bb">
+```
+
+Instantiating without a `val` argument will generate a random boolean value. This instance can now be used to interact your booleans. For example, to update your boolean's value, the API's `PUT` endpoint can be triggered by calling `#update`:
+
+```ruby
+bool.val = false
+bool.update
+```
+
+Or,
+
+```ruby
+bool.update false
+```
+
+Already have an ID? Retrieving your booleans is simple:
+
+```ruby
+bool = Boolio.fetch("5b4851a0-0883-4cef-8651-59b2daf053da")
+# => #<Boolio:0x007fdcfb3bb7a0 @val=true, @id="5b4851a0-0883-4cef-8651-59b2daf053da">
+```
+
+Invoking `#destroy!` removes your boolean from the booleans.io server, nullifies the instance's attributes, and freezes the instance:
+
+```ruby
+bool = Boolio.new val: false
+# => #<Boolio:0x007fdcf99e8b98 @val=false, @id="b3f0a1fb-b74a-42b0-a950-cd57bd29a351">
+bool.destroy!
+# => #<Boolio:0x007fdcf99e8b98 @val=nil, @id=nil>
+```
 
 ## Development
 
--- lib/boolio.rb
+++ lib/boolio.rb
@@ -1,7 +1,6 @@
-require "uri"
 require "json"
-require "net/http"
-# require "boolio/version"
+require "http"
+require "boolio/version"
 
 class Boolio
   attr_reader   :id
@@ -9,30 +8,56 @@ class Boolio
 
   URL = "https://api.booleans.io/"
 
-  def initialize(opts={})
-    @val = opts[:val] || [true, false].sample
-    @id  = opts[:id] || JSON.parse(post @val)["id"]
+  def initialize(opts = {})
+    @val = process_init_value(opts[:val])
+    @id  = opts[:id] || post(@val)["id"]
   end
 
-  def put(val)
-    request = Net::HTTP::Put.new("/#{id}")
-    request.set_form_data({ val: val })
-    response = Net::HTTP.new(URI.parse(URL)).request(request)
+  def update(value = val)
+    put(value)
   end
 
-  def post(val)
-    response = Net::HTTP.post_form(URI.parse(URL), { val: val })
-    response.body
-  end
-
-  def delete(id)
-    response = Net::HTTP.new(URI.parse(URL)).delete("#{id}")
-    response.body
+  def destroy!
+    return if frozen?
+    response = delete
+    if response.code == 200
+      @id  = nil
+      @val = nil
+      freeze
+    else
+      raise "Could not destroy boolean: #{response.status}"
+    end
   end
 
   def self.fetch(id)
-    response = Net::HTTP.get_response(URI.parse(URL + "#{id}"))
-    boolean = JSON.parse response.body
+    response = HTTP.get(URL + id)
+    boolean  = JSON.parse response.body
     Boolio.new(id: boolean["id"], val: boolean["val"])
+  end
+
+  def to_s
+    val
+  end
+
+  private
+
+  def process_init_value(value)
+    return [true, false].sample if value.nil?
+    raise ArgumentError, "Provide a boolean." unless [TrueClass, FalseClass, String].include?(value.class)
+    value.to_s == "true"
+  end
+
+  def put(value)
+    response = HTTP.put((URL + id), form: { val: value })
+    JSON.parse response.body
+  end
+
+  def post(value)
+    response = HTTP.post(URL, form: { val: value })
+    JSON.parse response.body
+  end
+
+  def delete
+    HTTP.delete(URL + id)
   end
 end
